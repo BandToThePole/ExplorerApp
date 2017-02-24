1f4acb77367a96835e2794f549bffddb6ade1382
Thomas Denney <me@thomasdenney.co.uk>
2017-02-24 23:47:22 +0000

    Add unit tests

---
+++ norway/NorwayTests/NorwayTests.m
@@ -0,0 +1,39 @@
+//
+//  NorwayTests.m
+//  NorwayTests
+//
+//  Created by Thomas Denney on 24/02/2017.
+//  Copyright © 2017 thomasdenney. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NorwayTests : XCTestCase
+
+@end
+
+@implementation NorwayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
